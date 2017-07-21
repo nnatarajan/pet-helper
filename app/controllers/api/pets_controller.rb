23484b841afd366ea8615a316fd639f165be9802
Natasha Natarajan <natashanatarajan@gmail.com>
2017-07-20 23:28:41 -0600

    updated pets and reminders controllers

--- app/controllers/api/pets_controller.rb
+++ app/controllers/api/pets_controller.rb
@@ -1,16 +1,42 @@
-class Api::PetsController < ApplicationController
+class Api::PetsController < petlicationController
+  before_action :set_pet, only: [:show, :update, :destroy]
+
   def index
+    render json: Pet.all.order(created_at: :desc)
   end
 
   def show
+    render json: @pet
   end
 
   def create
+    pet = Pet.create(pet_params)
+    if pet.save
+      render json: pet
+    else
+      render json: { errors: pet.errors.full_messages.join(',') }, status: 422
+    end
   end
 
   def update
+    if @pet.update(pet_params)
+      render json: @pet
+    else
+      render json: { errors: @pet.errors.full_messages.join(',') }, status: 422
+    end
   end
 
   def destroy
+      @pet.destroy
   end
+
+  private
+    def set_pet
+      @pet = pet.find(params[:id])
+    end
+
+    def pet_params
+      params.require(:pet).permit(:name, :species, :birthday)
+    end
 end
+
--- app/controllers/api/reminders_controller.rb
+++ app/controllers/api/reminders_controller.rb
@@ -1,16 +1,40 @@
 class Api::RemindersController < ApplicationController
+
   def index
+    render json: Reminder.all.order(created_at: :desc)
   end
 
   def show
+    render json: @reminder
   end
 
   def create
+    reminder = Reminder.create(reminder_params)
+    if reminder.save
+      render json: reminder
+    else
+      render json: { errors: reminder.errors.full_messages.join(',') }, status: 422
+    end
   end
 
   def update
+    if @reminder.update(reminder_params)
+      render json: @reminder
+    else
+      render json: { errors: @reminder.errors.full_messages.join(',') }, status: 422
+    end
   end
 
   def destroy
+      @reminder.destroy
   end
+
+  private
+    def set_reminder
+      @reminder = Reminder.find(params[:id])
+    end
+
+    def reminder_params
+      params.require(:reminder).permit(:type, :date_time, :repeat, :repeat_pattern)
+    end
 end
