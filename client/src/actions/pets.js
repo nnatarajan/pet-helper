7d332de676cbd15347ba6cb0c0e6c1d4a7c01f45
Natasha Natarajan <natashanatarajan@gmail.com>
2017-07-30 14:03:43 -0600

    adding redux, setup pets file under actions

---
+++ client/src/actions/pets.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+export const addPet = (pet) => {
+  return(dispatch) => {
+    const { name, species, birthday, notes} = pet;
+    // TODO: Add user id?
+    axios.post(`/api/pets`, { pet: { name, species, birthday, notes } })
+    .then( res => {
+      dispatch({ type: 'ADD_PET', pet: res.data });
+      console.log(res)
+      console.log("Request to AddPet Succeeded :)")
+    })
+    .catch( res => {
+      console.log(res)
+      console.log("Request to AddPet Failed :(")
+    });
+  }
+}
+
+export const fetchPets = () => {
+  return(dispatch) => {
+    axios.get(`/api/pets`)
+    .then( (res) => {
+      dispatch( { type: 'GET_PETS', pets: res.data});
+      console.log("REQUEST SUCCEEDED");
+      console.log(res);
+    }).catch (res) => {
+      console.log("REQUEST FAILED");
+      console.log(res);
+    });
+  }
+}
