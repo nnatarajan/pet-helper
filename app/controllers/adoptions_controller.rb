6f8f91abf80bfb20fd27ab9f92a44a583299a37f
Natasha Natarajan <natashanatarajan@gmail.com>
2017-08-01 23:05:50 -0600

    update gitignore, added api to adoptions controller

--- app/controllers/adoptions_controller.rb
+++ app/controllers/adoptions_controller.rb
@@ -1,2 +1,5 @@
 class AdoptionsController < ApplicationController
+  def index
+    res = HTTParty.get("http://api.petfinder.com/shelter.find?key=1ac766f7e5e531366f2b62714ed2e4c2&location=84111&format=json")
+  end
 end
