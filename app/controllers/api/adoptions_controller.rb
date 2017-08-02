39caf817f1c28c784b4b322b3a0cf4a5dbbc85a3
Natasha Natarajan <natashanatarajan@gmail.com>
2017-08-02 04:15:25 -0600

    modified api count to prevent system from crashing

--- app/controllers/api/adoptions_controller.rb
+++ app/controllers/api/adoptions_controller.rb
@@ -1,6 +1,6 @@
 class Api::AdoptionsController < ApplicationController
   def index
-    res = HTTParty.get("http://api.petfinder.com/shelter.find?key=1ac766f7e5e531366f2b62714ed2e4c2&location=84111&count=100&format=json")
+    res = HTTParty.get("http://api.petfinder.com/shelter.find?key=1ac766f7e5e531366f2b62714ed2e4c2&location=84111&count=50&format=json")
 
     render json: res
   end
