41af88eeee521a7067bda83eafa145f9fdaa3a65
Natasha Natarajan <natashanatarajan@gmail.com>
2017-08-23 10:18:02 -0600

    test manage pet with 800 X 400

--- client/src/components/ManagePet.js
+++ client/src/components/ManagePet.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Header, Image, Button, Card, Table} from 'semantic-ui-react';
 import { fetchSelectedPet } from '../actions/pets';
-import Kitten from '../images/kitten_snow.jpeg';
+import Kitten from '../images/kitten_blue_eyes.jpeg';
 import { Row, Col, Container } from 'react-materialize';
 
 class ManagePet extends Component {
