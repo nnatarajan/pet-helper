f007e187e67762c3e93654567183852d5dcc8a36
Natasha Natarajan <natashanatarajan@gmail.com>
2017-08-22 23:10:58 -0600

    updated my pets card to fluid

--- client/src/components/Pets.js
+++ client/src/components/Pets.js
@@ -18,7 +18,7 @@ class Pets extends Component {
     return this.props.pets.map(pet => {
       return (
 
-        <Card className='cyan lighten-5'>
+        <Card fluid className='cyan lighten-5'>
 
           <Card.Content>
             <Card.Header>{pet.name}</Card.Header>
