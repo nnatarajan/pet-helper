d458f1f65e9ced8085e984f432286cdeec495278
Natasha Natarajan <natashanatarajan@gmail.com>
2017-10-16 14:15:18 -0600

    about wording

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -6,52 +6,44 @@ import Cat from '../images/about.jpeg';
 
 const About = () => (
   <Container text>
+    <Row>
+      <Col s={12}>
+      <br />
+        <Header textAlign='center'><h1><strong>About</strong></h1></Header>
+      </Col>
+    </Row>
+    <Row>
+      <Col s={12}>
+      <Image src={Cat} alt='Cat' centered fluid/>
+      </Col>
+    </Row>
+    <Row >
+      <Col s={12}>
+      <br />
+        <p textAlign='center'>
+        Hello! Welcome to Pet Helper! I have designed and built this web application with the sole purpose of assisting pet owners in taking the best care of their pets. The main features of this app are finding a new pet, storing & viewing pet information, and setting up reminders. Currently, I am working on adding more features and making improvements to Pet Helper. Please note that you may need to delete your browser history or change to a different browser to view the most current version of the Pet Helper application.
+        </p>
+      </Col>
+    </Row>
+    <Row>
+      <Col s={12}>
+        <p>
+        On launch day (August 2017), a veterinary assistant approached my booth and offered me welcomed suggestions. He had stated that the most important part of pet care is immunizations. In addition, he had mentioned that too often pet owners forget how vital immunizations are for basic care. Please click on this <a href="https://www.aspca.org/pet-care/general-pet-care/vaccinations-your-pet"> link </a> for more detailed information from the ASPCA. If you are considering adopting a pet, I have included a built-in API, that reflects up-to-date contact information for a list of local  <Link to='/adoptions'>  animal shelters </Link> in Salt Lake City, UT and surrounding areas. Lastly, please check out my <a href="https://nnatarajan.github.io/"> website </a>for upcoming projects.
+        </p>
+      </Col>
+    </Row>
+    <Row>
+      <Col s={12}>
+      <p>Thank you for visiting my site! </p>
+      </Col>
+    </Row>
+    <Row>
+      <Col s={12}>
+      <p>~ Natasha</p>
+      </Col>
+    </Row>
 
-<Row>
-<Col s={12}>
-<br />
-<Header textAlign='center'>
-<h1><strong>About</strong></h1>
-</Header>
-</Col>
-</Row>
-<Row>
-  <Col s={12}>
-  <Image src={Cat} alt='Cat' centered fluid/>
-  </Col>
-</Row>
-<Row >
-<Col s={12}>
-<br />
-<p textAlign='center'>
-Hello! Welcome to Pet Helper! I have designed and built this web application with the sole purpose of assisting pet owners in taking the best care of their pets. The main features of this app are finding a new pet, storing & viewing pet information, and setting up reminders. Currently, I am working on adding more features and making improvements to Pet Helper. Please note that you may need to delete your browser history or change to a different browser to view the most current version of the app.
-</p>
-</Col>
-</Row>
-<Row>
-<Col s={12}>
-<p>
-On launch day (August 2017), a veterinary assistant approached my booth and offered me welcomed suggestions. He had stated that the most important part of pet care is immunizations. In addition, he had mentioned that too often pet owners forget how vital immunizations are for basic care. Please click on this <a href="https://www.aspca.org/pet-care/general-pet-care/vaccinations-your-pet"> link </a> for more detailed information from the ASPCA. If you are considering adopting a pet, I have included a built-in API, that reflects up-to-date contact information for a list of local  <Link to='/adoptions'>  animal shelters </Link> in the Salt Lake City area. Lastly, please check out my <a href="https://nnatarajan.github.io/"> website </a>for upcoming projects.
-</p>
-</Col>
-</Row>
-<Row>
-<Col s={12}>
-<p>
-Thank you for visiting my site.
-</p>
-</Col>
-</Row>
+  </Container>
 
-<Row>
-<Col s={12}>
-<p>
-- Natasha
-</p>
-</Col>
-</Row>
-
-</Container>
 )
-
 export default About
